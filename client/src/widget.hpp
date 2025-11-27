1b2e63a59e9a387bc42df3a858ed33de6e66506f
etorth <anhonghe@gmail.com>
2025-11-27 02:32:10 -0500

    f

--- client/src/widget.hpp
+++ client/src/widget.hpp
@@ -369,28 +369,7 @@ class Widget: public WidgetTreeNode
         template<typename T> static bool execCheckFunc(const Widget::VarCheckFunc<T> &, const Widget *, void *, const T &);
 
     private:
-        class RecursionDetector final
-        {
-            private:
-                bool &m_flag;
-
-            public:
-                RecursionDetector(bool &flag, const char *type, const char *func)
-                    : m_flag(flag)
-                {
-                    if(m_flag){
-                        throw fflerror("recursion detected in %s::%s", type, func);
-                    }
-                    else{
-                        m_flag = true;
-                    }
-                }
-
-                ~RecursionDetector()
-                {
-                    m_flag = false;
-                }
-        };
+#include "widget.recursion.hpp"
 
     private:
         Widget::VarDir m_dir;
